af56c846584774dae8a1aed6a2a864db5f15f563
yuvalzh <yuvi.zehavi@gmail.com>
2022-06-26 02:25:28 +0300

    Update views.py
    remove redundant comments

--- matcherProject/views.py
+++ matcherProject/views.py
@@ -4,16 +4,16 @@ from matcherProject.services.candidates_service import find_best_candidates
 # Create your views here.
 
 def handle_candidates_query (request):
-    # get params
+    # get param from request
     job_title = request.GET.get('job_title')
+    
     # validate them 
     if not job_title:
         return HttpResponse('not valid job title')
-    # send to BL action  --> find_best_candidates(job)
     try:
-
         candidates  = find_best_candidates(job_title)
     except Exception as err:
         return HttpResponse(err)
+
     # return the response
     return HttpResponse(candidates)
